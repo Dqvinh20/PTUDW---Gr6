59892bfda83e07e08d5120fe48c4f6913ea632a1
nguyenhau2506 <nvhau20@clc.fitus.edu.vn>
2022-12-13 10:55:12 +0700

    front-end teacher

--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -2,8 +2,19 @@ import express from "express";
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('home');
+router.get("/", function (req, res, next) {
+    res.render("home");
+});
+
+router.get("/teacher", function (req, res, next) {
+    res.render("teacher/index", { layout: "teacherLayout" });
+});
+
+router.get("/teacher/teacher-profile", function (req, res, next) {
+    res.render("teacher/teacher-profile", { layout: "teacherLayout" });
+});
+router.get("/teacher/course", function (req, res, next) {
+    res.render("teacher/course", { layout: "teacherLayout" });
 });
 
 export default router;

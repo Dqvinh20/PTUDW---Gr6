e0043c45e82d3d60ce1a5bf995a47e9c58d260ed
DESKTOP-HKGEPO2\duong <dqvinh20@clc.fitus.edu.vn>
2023-01-09 20:19:54 +0700

    Fix login, signup pop alert

--- public/js/alert.js
+++ public/js/alert.js
@@ -1,11 +1,16 @@
-const alertAnimation = () => {
-    const alert = $('.alert')
-    alert.css('right', alert.width);
-    alert.css('transition', "right 2s");
-    alert.css('right', "0");
+function alertAnimation() {
+    try{
+        const alert = $('.alert')
+        alert.css('right', alert.width);
+        alert.css('transition', "right 2s");
+        alert.css('right', "0");
 
-    const bsAlert = new bootstrap.Alert('.alert');
-    setTimeout(() => {
-        bsAlert.close();
-    }, 7000)
+        const bsAlert = new bootstrap.Alert('.alert');
+        setTimeout(() => {
+            bsAlert.close();
+        }, 7000)
+    }catch (err) {
+        console.log(err)
+    }
+
 }

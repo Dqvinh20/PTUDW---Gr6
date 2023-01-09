20312cee829fc0df6082b1e0f69851dea46eba19
DESKTOP-HKGEPO2\duong <dqvinh20@clc.fitus.edu.vn>
2023-01-09 22:56:31 +0700

    Add render teacher name

--- src/services/my_courses.service.js
+++ src/services/my_courses.service.js
@@ -1,18 +1,13 @@
 import db from "../utils/db.js";
 
-// for (let i = 1; i < 50; i++) {
-//     await db('courses_own').insert({student_id: 1, course_id: i});
-// }
-// for (let i = 1; i < 50; i++) {
-//     await db('watch_list').insert({student_id: 1, course_id: i});
-// }
-
 const getMyLearningCourse = async (studentId, offset, limit) => {
     const courses = db('courses')
         .join('courses_own', {'courses_own.course_id': "courses.id"})
         .join('categories', {'categories.cat_id' : 'courses.cat_id'})
+        .join('users', {"users.id" : "courses.teacher_id"})
         .where('courses_own.student_id', studentId)
-        .select(["courses.*", "categories.cat_name", "categories.parent_cat_id"]);
+        .select(["courses.*", "categories.cat_name", "categories.parent_cat_id", "users.fullname as teacher_name"]);
+
     if (offset) courses.offset(offset);
     if (limit) courses.limit(limit);
     await courses;
@@ -24,8 +19,9 @@ const getMyWatchList = async (studentId, offset, limit) => {
     const courses = db('courses')
         .join('watch_list', {'watch_list.course_id': "courses.id"})
         .join('categories', {'categories.cat_id' : 'courses.cat_id'})
+        .join('users', {"users.id" : "courses.teacher_id"})
         .where('watch_list.student_id', studentId)
-        .select(["courses.*", "categories.cat_name", "categories.parent_cat_id"]);
+        .select(["courses.*", "categories.cat_name", "categories.parent_cat_id", "users.fullname as teacher_name"]);
     if (offset) courses.offset(offset);
     if (limit) courses.limit(limit);
     await courses;
@@ -51,10 +47,18 @@ const countAllLearningCourses = async (studentId) => {
     return result[0].count;
 }
 
+const countAllWatchListCourses = async (studentId) => {
+    const result = await db('watch_list')
+        .where("student_id", studentId)
+        .count();
+    return result[0].count;
+}
+
 export default {
     getMyLearningCourse,
     getMyWatchList,
     removeFromWatchList,
     countAllLearningCourses,
+    countAllWatchListCourses,
     addToWatchList
 }
